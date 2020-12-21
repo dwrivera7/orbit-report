84a907a3a2ed1b4d5c2ec9887eb968d5c8394fb4
Dathan Rivera <dwrivera7@gmail.com>
2020-12-21 16:00:59 -0600

    Assignment 6 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,13 +37,14 @@ export class AppComponent {
  search(searchTerm: string): void {
   let matchingSatellites: Satellite[] = [];
   searchTerm = searchTerm.toLowerCase();
-  for(let i=0; i < this.sourceList.length; i++) {
-     let name = this.sourceList[i].name.toLowerCase();
-     if (name.indexOf(searchTerm) >= 0) {
-        matchingSatellites.push(this.sourceList[i]);
-     }
+  for (let i = 0; i < this.sourceList.length; i++) {
+    let name = this.sourceList[i].name.toLowerCase();
+    let orbitType = this.sourceList[i].orbitType.toLowerCase();
+    let type = this.sourceList[i].type.toLowerCase();
+    if (name.indexOf(searchTerm) >= 0 || orbitType.indexOf(searchTerm) >= 0 || type.indexOf(searchTerm) >= 0) {
+      matchingSatellites.push(this.sourceList[i]);
+    }
   }
   this.displayList = matchingSatellites;
 }
-
 }
