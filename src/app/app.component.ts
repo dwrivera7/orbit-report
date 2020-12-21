335bf35ee430220c395268476339358144beee00
Dathan Rivera <dwrivera7@gmail.com>
2020-12-21 14:41:47 -0600

    Step 8 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,11 @@ export class AppComponent {
   title = 'orbit-report';
 
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -26,8 +28,22 @@ export class AppComponent {
               fetchedSatellites[i].orbitType,
               fetchedSatellites[i].operational);
             this.sourceList.push(satellite);
+            this.displayList = this.sourceList.slice(0);
           }
         }.bind(this));
     }.bind(this)); 
  }
+
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  this.displayList = matchingSatellites;
+}
+
 }
